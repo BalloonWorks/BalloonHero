05269d908ee64f593d9de7732c6d9ee5872fc6fb
Christopher Chianelli <chris.chianelli@gmail.com>
2016-01-29 01:50:16 -0500

    Added files for wind.
    World - Gets input from player and store global variables.
    
    WindForce - Applies wind to an object.
    
    SnowParticles - Applies wind to snow particles.
    
    Util - Contain utility methods (currently one for setting a value to be
    a percentage between two values)

---
+++ Scripts/SnowParticles.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using AssemblyCSharp;
+
+/// <summary>
+/// Updates the snow particles to react to the wind.
+/// </summary>
+public class SnowParticles : MonoBehaviour {
+
+	public ParticleSystem snow;
+	
+	// Update is called once per frame
+	void Update () {
+		ParticleSystem.Particle[] p = new ParticleSystem.Particle[snow.particleCount+1];
+		int l = snow.GetParticles(p);
+
+		int i = 0;
+		while (i < l) {
+			p[i].velocity = new Vector3(-1*Util.percentDiff(-1*p[i].velocity.x,World.data.WindSpeed(),0.05f),
+				p[i].velocity.y, p[i].velocity.z);
+			i++;
+		}
+
+		snow.SetParticles(p, l);  
+	}
+}
---
+++ Scripts/World.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// World: Stores infomation pertaining to the world that other
+/// classes require.
+/// </summary>
+public class World : MonoBehaviour {
+
+	/// <summary>
+	/// Points to the instance of the world, used to get
+	/// data about the world.
+	/// </summary>
+	public static World data;//Points to the instance of the world
+
+	/// <summary>
+	/// The current global wind speed.
+	/// </summary>
+	private float windSpeed;
+
+	/// <summary>
+	/// Destroy this instance if one exists, otherwise
+	/// intializes it.
+	/// </summary>
+	void Start () {
+		//If there is no World...
+		if (data == null) {
+			//Set the static instance to this object
+			data = this;
+		} 
+		//Otherwise...
+		else {
+			//An instance exists, so destroy it
+			Destroy (this.gameObject);
+		}
+	}
+	
+	/// <summary>
+	/// Captures any revelant user input and update fields. accordingly.
+	/// </summary>
+	void Update () {
+		windSpeed = Input.GetAxis ("Horizontal");
+	}
+
+	/// <summary>
+	/// Get the global windspeed.
+	/// </summary>
+	/// <returns>The current windspeed.</returns>
+	public float WindSpeed(){
+		return 3*windSpeed;
+	}
+}
