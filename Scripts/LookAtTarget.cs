15bd0214bdd64991341743621f46611612e19f51
Christopher Chianelli <chris.chianelli@gmail.com>
2016-02-05 22:57:27 -0500

    Added files for floating, looking, and destroying on click. Fixed lightness in WindForce.
    -DestroyOnClick: Destroys the object it is attached to when clicked on
    by the mouse.
    
    -Float: Apply a constant up force (until a specified speed limit) to an
    object.
    
    -LookAtTarget: Rotates the object it is attached to towards a target
    object.
    
    -WindForce: Fixed a bug where lightness is ignored.

---
+++ Scripts/LookAtTarget.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using AssemblyCSharp;
+
+public class LookAtTarget : MonoBehaviour {
+
+	/// <summary>
+	/// The target to look at.
+	/// </summary>
+	public Transform target;
+
+
+	/// <summary>
+	/// Rotation speed.
+	/// </summary>
+	public float speed;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt 
+		(new Vector3(
+			Util.percentDiff(transform.position.x,target.position.x,speed),
+			Util.percentDiff(transform.position.y,target.position.y,speed),
+			Util.percentDiff(transform.position.z,target.position.z,speed))
+		);
+	}
+}
