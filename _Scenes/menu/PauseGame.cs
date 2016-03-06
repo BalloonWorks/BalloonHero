4dbe6e476b9784703cd905b9bb3d6aab0684f9ed
Christopher Chianelli <chris.chianelli@gmail.com>
2016-03-06 18:03:38 -0500

    Added Scenes back.

---
+++ _Scenes/menu/PauseGame.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseGame : MonoBehaviour {
+
+	public GameObject pauseMenu;
+	
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.P)) {
+			if (Time.timeScale == 1.0F) { //pause the game
+				Time.timeScale = 0.0F;
+				pauseMenu.SetActive (true);
+			} else {  //un-pause
+				Time.timeScale = 1.0F;
+				pauseMenu.SetActive (false);
+			}
+		}
+	
+	}
+
+	/* Use for other pausing methods. */
+	public void pauseUnPause(){
+		if (Time.timeScale == 1.0F) { //pause the game
+			Time.timeScale = 0.0F;
+			pauseMenu.SetActive (true);
+		} else {  //un-pause
+			Time.timeScale = 1.0F;
+			pauseMenu.SetActive (false);
+		}
+	}
+}
