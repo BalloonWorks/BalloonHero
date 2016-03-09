ee3f2d541bc6e59037e4a1724f428ff667499a2e
Christopher Chianelli <chris.chianelli@gmail.com>
2016-03-08 22:30:45 -0500

    Implemented Dove Feature.

---
+++ HighlightBalloonTargets.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HighlightBalloonTargets : MonoBehaviour {
+
+	public void HighlightTargets(){
+		World.data.SetAddingBalloon ();
+	}
+}
